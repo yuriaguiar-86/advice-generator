458f408dedcbf905551e7e098417fb37f1a9344a
yuriaguiar-86 <kakacraft337@gmail.com>
2023-02-03 14:22:22 -0300

    add rel no link

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,7 +3,7 @@ import './footer.css';
 const Footer = () => {
     return (
         <footer class="attribution">
-            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>. 
             Coded by <a href="https://github.com/yuriaguiar-86">Yuri Aguiar</a>.
         </footer>
     );
